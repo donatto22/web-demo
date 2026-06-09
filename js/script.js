6015bafec256f8e199d15310fa7fd12f9c8bd45b
donatto22 <ottanod22@gmail.com>
2026-06-08 23:04:08 -0500

    clase lunes 08

--- js/script.js
+++ js/script.js
@@ -125,55 +125,164 @@
 // // edad > 18 ? if : else
 
 
-// //-------------------------------
+// //------------------------------- clase viernes pasado
 
-function areaTriangulo(base, altura) {
-    console.log((base * altura) / 2)
+// function areaTriangulo(base, altura) {
+//     console.log((base * altura) / 2)
+// }
+
+// areaTriangulo(10, 5)
+// areaTriangulo(20, 100)
+// areaTriangulo(5, 7)
+// areaTriangulo(13, 4)
+// areaTriangulo(9, 25)
+
+// function verificarIngreso(edad) {
+//     if (edad >= 18) {
+//         console.log("Puedes ingresar")
+//     } else {
+//         console.log("No Puedes ingresar, eres menor aún")
+//     }
+// }
+
+// verificarIngreso(21)
+// verificarIngreso(19)
+// verificarIngreso(16)
+// verificarIngreso(13)
+
+// function calcularPrecio(precioBase, descuento) {
+//     const precioFinal = precioBase - (precioBase * descuento / 100)
+//     const ahorro = precioBase - precioFinal
+
+//     console.log("El precio final es:" + precioFinal)
+//     console.log("El ahorro es:" + ahorro)
+// }
+
+// calcularPrecio(120, 10)
+
+// function calculadoraBasica(num1, num2, operador) {
+//     if (operador == '+') console.log(num1 + num2)
+//     else if (operador == '-') console.log(num1 - num2)
+//     else if (operador == '*') console.log(num1 * num2)
+//     else if (operador == '/') {
+//         if (num2 == 0) {
+//             console.log("No puedes dividir entre 0")
+//         } else {
+//             console.log(num1 / num2)
+//         }
+//     }
+//     else {
+//         console.log("Operación invalida")
+//     }
+// }
+
+// calculadoraBasica(20, 0, '/')
+
+// -------------------------------------
+let nombre = "Ana"
+// convierte el texto a mayúsculas
+console.log(nombre.toUpperCase())
+
+let frase = "HOY PERÚ GANA"
+// coloca en minúsculas 
+console.log(frase.toLowerCase())
+
+// limpiamos / eliminamos los espacios en los externos
+let frase2 = " esta es una frase "
+console.log(frase2.trim())
+
+// verificar si existe una palabra
+// dentro del string
+let frase3 = "Bienvenidos a Javascript"
+console.log(frase3.includes("Java"))
+
+let fraseParaOlvidarla = 'Una ves recibí un consejo que decía "Para olvidarla..." que fue de mi amigo'
+
+// slice para sacar una portción del texto
+console.log(fraseParaOlvidarla.slice(0, 10))
+
+// necesito reemplazar un texto con otro
+console.log(fraseParaOlvidarla.replace("olvidarla", "conquistarla"))
+
+// split - Separa una cadena de texto y lo convierte
+// en un arreglo mediante un "delimitador"
+// split busca en el texto donde esté el caracter que
+// tú le indiques, por cada vez que lo encuentre
+// creará un item dentro de tu cajita que es tu arreglo
+let csv = "Ana,Luis,Carlos,Pepe"
+console.log(csv.split(","))
+
+// me gustaría saber si un texto inicia o termina
+// con cierta palabra o frase
+let frase4 = "Hay 28 alumnos presentes"
+console.log(frase4.startsWith("Hay"))
+console.log(frase4.endsWith("presentes"))
+
+
+// textos - string
+// numeros - number
+// true / false - boolean
+
+// ---------------------- NUMEROS
+let precio = 15.123879
+
+// redondera los decimales
+console.log(precio.toFixed(3))
+
+// convertir un numero a texto
+console.log("15.123879")
+
+let pozoGanador = 1000000
+console.log(pozoGanador.toLocaleString())
+
+// verificar si algo No es un numero
+console.log(Number.isNaN("25"))
+// NaN - Not a Number
+
+// --------------------------
+// Arreglo / Array
+let frutas = ["fresa", 123, "lima", "mandarina", "limon"]
+
+// agregando un item (ultimo)
+frutas.push("maracuyá")
+console.log(frutas)
+
+// agregando un item (primero)
+frutas.unshift("cereza")
+console.log(frutas)
+// fresa, naranja, mandarina, limon,
+
+// eliminando frutas (ultimo)
+frutas.pop()
+console.log(frutas)
+
+// eliminando frutas (primero)
+frutas.shift()
+console.log(frutas)
+
+// revertir un arreglo
+frutas.reverse()
+console.log(frutas)
+
+// convertir un arreglo a string
+// juntando cada item con algún caracter
+console.log(frutas.join(','))
+
+// console.log(frutas[(frutas.length / 2).toFixed(0) - 1])
+
+const nombres = ["pepe", "ana", "juan", "maria"]
+
+
+for (let i = 0; i < nombres.length; i++) {
+    console.log(nombres[i].toUpperCase())
 }
 
-areaTriangulo(10, 5)
-areaTriangulo(20, 100)
-areaTriangulo(5, 7)
-areaTriangulo(13, 4)
-areaTriangulo(9, 25)
+console.log("---------------------")
 
-function verificarIngreso(edad) {
-    if (edad >= 18) {
-        console.log("Puedes ingresar")
-    } else {
-        console.log("No Puedes ingresar, eres menor aún")
+nombres.forEach((nombre) => {
+    if (nombre == "ana") {
+        console.log(nombre.toUpperCase())
     }
-}
+})
 
-verificarIngreso(21)
-verificarIngreso(19)
-verificarIngreso(16)
-verificarIngreso(13)
-
-function calcularPrecio(precioBase, descuento) {
-    const precioFinal = precioBase - (precioBase * descuento / 100)
-    const ahorro = precioBase - precioFinal
-
-    console.log("El precio final es:" + precioFinal)
-    console.log("El ahorro es:" + ahorro)
-}
-
-calcularPrecio(120, 10)
-
-function calculadoraBasica(num1, num2, operador) {
-    if (operador == '+') console.log(num1 + num2)
-    else if (operador == '-') console.log(num1 - num2)
-    else if (operador == '*') console.log(num1 * num2)
-    else if (operador == '/') {
-        if (num2 == 0) {
-            console.log("No puedes dividir entre 0")
-        } else {
-            console.log(num1 / num2)
-        }
-    }
-    else {
-        console.log("Operación invalida")
-    }
-}
-
-calculadoraBasica(20, 0, '/')
+nombres = 123
